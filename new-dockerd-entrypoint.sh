ac7d9f94608cdcab5c6c6f992c8f9516a33f6d08
BenEvenTsur <38428408+BenEvenTsur@users.noreply.github.com>
2020-11-25 16:49:17 +0200

    Update new-dockerd-entrypoint.sh

--- new-dockerd-entrypoint.sh
+++ new-dockerd-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -jar /opt/agent.jar -jnlpUrl $JNLPURL -secret $JENKINS_SECRET -workDir "$HOME_DIR"
+java -jar /opt/agent.jar -jnlpUrl http://$JENKINS_ADDRESS/computer/$AGENT_NAME/slave-agent.jnlp -secret $JENKINS_SECRET -workDir "$HOME_DIR" &
 
 dockerd-entrypoint.sh
